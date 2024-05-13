88b49b11ac7534ae7e7a86d5487f2ebd126060b5
Andy Kreling <krelinga@gmail.com>
2024-05-13 03:37:22 +0000

    main() starts RPC server.

--- main.go
+++ main.go
@@ -1,4 +1,31 @@
 package main
 
+import (
+    "log"
+    "net"
+
+    "github.com/krelinga/rsync-server/pb"
+    "google.golang.org/grpc"
+)
+
+type RsyncServer struct {
+    pb.UnimplementedRsyncServer
+}
+
+func MainOrError() error {
+    lis, err := net.Listen("tcp", ":25002")
+    if err != nil {
+        return err
+    }
+    grpcServer := grpc.NewServer()
+    pb.RegisterRsyncServer(grpcServer, &RsyncServer{})
+    grpcServer.Serve(lis)  // Runs as long as the server is alive.
+
+    return nil
+}
+
 func main() {
+    if err := MainOrError(); err != nil {
+        log.Fatal(err)
+    }
 }
