7e4bf9d4b283ff42432f050981542b955dbee588
Andy Kreling <krelinga@gmail.com>
2024-07-22 01:51:39 +0000

    remove fancy stuff that didn't work, log raw rsync outputs.

--- copy.go
+++ copy.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-    "bufio"
     "context"
     "fmt"
+    "io"
     "log"
     "os/exec"
-    "syscall"
-    "time"
+    "bytes"
 
     "github.com/krelinga/rsync-server/pb"
 )
@@ -23,40 +22,20 @@ func copyImpl(ctx context.Context, req *pb.CopyRequest) (*pb.CopyReply, error) {
     }
     args = append(args, req.InPath, req.OutSuperPath)
     cmd := exec.CommandContext(ctx, "rsync", args...)
-    pipe, err := cmd.StdoutPipe()
-    if err != nil {
-        return nil, fmt.Errorf("Could not get stdout pipe: %w", err)
-    }
+    pipe := &bytes.Buffer{}
+    cmd.Stdout = pipe
     if err := cmd.Start(); err != nil {
         return nil, fmt.Errorf("Could not start process: %w", err)
-    }
-    s := bufio.NewScanner(pipe)
-    proc := cmd.Process
-    time.Sleep(time.Second)
-    requestOutput := func() {
-        if proc != nil {
-            // there's a race where cmd.Process could become nil before we can
-            // grab it for very short-running commands, so we need to check
-            // for that here.
-
-            // Send a signal to force rsync to output a progress line after the
-            // current file is completed.
-            // Swallow the error, if any.
-            _ = proc.Signal(syscall.SIGVTALRM)
-        }
-    }
-    requestOutput()
-    for s.Scan() {
-        line := s.Text()
-        log.Println(line)
-        requestOutput()
-    }
-    if err := s.Err(); err != nil {
-        return nil, fmt.Errorf("Could not finish scanner: %w", err)
     }
     if err := cmd.Wait(); err != nil {
         return nil, fmt.Errorf("Error waiting for command to finish: %w", err)
     }
+    contents, err := io.ReadAll(pipe)
+    if err != nil {
+        return nil, fmt.Errorf("Could not read from pipe: %w", err)
+    }
+    log.Printf("Read %d bytes from pipe.\n", len(contents))
+    log.Printf("Contents: %q\n", contents)
 
     return &pb.CopyReply{}, nil
 }
